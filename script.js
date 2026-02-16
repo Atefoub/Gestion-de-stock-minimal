d8d38195b3a7ff5208fcc727b79131ab7d9670b2
Antoine Mourin <antoinem1pro@gmail.com>
2026-02-16 13:42:35 +0100

    exercice ok

---
+++ script.js
@@ -0,0 +1,35 @@
+const products = [
+  { name: "Stylo", stock: 10 },
+  { name: "Livre", stock: 3 },
+  { name: "Clavier", stock: 0 }
+];
+
+
+function stockStatus(products) {
+  const result = [];
+
+  for (const product of products) {
+    let status;
+
+    if (product.stock === 0) {
+      status = "out";
+    } else if (product.stock < 5) {
+      status = "low";
+    } else {
+      status = "ok";
+    }
+
+    result.push({
+      name: product.name,
+      stock: product.stock,
+      status: status
+    });
+  }
+
+  return result;
+}
+
+
+
+const result = stockStatus(products);
+console.log(result);
